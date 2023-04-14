2ce75995f805614ed17e0cff285001eea45d81f5
ehrun32 <aruncheriakara@gmail.com>
2023-04-14 13:15:11 -0400

    update icons and names

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -48,6 +48,7 @@ const Invoices = () => {
       <Box
         m="40px 0 0 0"
         height="75vh"
+        width="100%"
         sx={{
           "& .MuiDataGrid-root": {
             border: "none",
